54f59837633b2a0da7ce7c65d576e5c9e3cfc524
MelLutten <57198988+MelLutten@users.noreply.github.com>
2019-10-31 18:19:07 +0100

    Update terrain.cpp

--- terrain.cpp
+++ terrain.cpp
@@ -55,23 +55,42 @@ void terrain::InitialisationGrille(){
     int compteurRobot1G = 0;
     int compteurRobot2G = 0;
     int compteurDebris = 0;
+    int compteurZero = 0;
 
+    srand(time(NULL));
 
-
-    for(int i=1;i<d_taille2;++i){
-        for(int j=1;j<d_taille1;++j){
+    for(int i=0;i<d_taille2;++i){
+        for(int j=0;j<d_taille1;++j){
 
             int indice = rand()%static_cast<int>(V.size());
             int nbalea = V[static_cast<unsigned>(indice)];
 
-             d_grille[static_cast<unsigned>(i)][static_cast<unsigned>(j)] = nbalea;
-
+            d_grille[static_cast<unsigned>(i)][static_cast<unsigned>(j)] = nbalea;
+            
+            if(nbalea==0){++compteurZero;}
             if(nbalea==1){++compteurJoueur;}
             if(nbalea==2){++compteurRobot1G;}
             if(nbalea==3){++compteurRobot2G;}
             if(nbalea==4){++compteurDebris;}
+            
 
+            if(compteurZero==(d_taille1*d_taille2)-(d_nbdebris+d_nbrobotFirstG+d_nbrobotSecondG+1)){
 
+                for(int g=0;g<static_cast<int>(V.size());++g){
+
+                    if(V[static_cast<unsigned>(g)]==0){
+                        for(int r=g;r<static_cast<int>(V.size());++r){
+ 
+                                 V[static_cast<unsigned>(r)]=V[static_cast<unsigned>(r+1)];
+                            
+                        }
+                    }
+                }
+
+                 V.resize(V.size()-1);
+                 compteurZero = ((d_taille1*d_taille2)-(d_nbdebris+d_nbrobotFirstG+d_nbrobotSecondG+1))+1;
+            }
+            
 
             if(compteurRobot1G==d_nbrobotFirstG){
 
@@ -86,7 +105,7 @@ void terrain::InitialisationGrille(){
                 }
 
                  V.resize(V.size()-1);
-                 compteurRobot1G = 6;
+                 compteurRobot1G = d_nbrobotFirstG+1;
             }
             if(compteurRobot2G==d_nbrobotSecondG){
 
@@ -101,7 +120,7 @@ void terrain::InitialisationGrille(){
                 }
 
                  V.resize(V.size()-1);
-                 compteurRobot2G = 6;
+                 compteurRobot2G = d_nbrobotSecondG+1;
 
 
             }
@@ -118,7 +137,7 @@ void terrain::InitialisationGrille(){
                 }
 
                  V.resize(V.size()-1);
-                 compteurDebris = 6;
+                 compteurDebris = d_nbdebris+1;
 
             }
 
@@ -136,7 +155,7 @@ void terrain::InitialisationGrille(){
 
 
                  V.resize(V.size()-1);
-                 compteurJoueur = 6;
+                 compteurJoueur = 2;
 
             }
 
