681a384205062a57592550571572502789bb139f
MelLutten <57198988+MelLutten@users.noreply.github.com>
2019-10-31 14:38:25 +0100

    Update terrain.cpp

--- terrain.cpp
+++ terrain.cpp
@@ -6,7 +6,6 @@ terrain::terrain(){
 
 terrain::terrain(int nbdebris,int nbrobotfirstG, int nbrobotsecondG, int taille1, int taille2):d_nbdebris{nbdebris},d_nbrobotFirstG{nbrobotfirstG},d_nbrobotSecondG{nbrobotsecondG},d_taille1{taille1},d_taille2{taille2}
 {
-
     sauverTerrain("premiereSauvegarde.txt");
 }
 
@@ -28,8 +27,9 @@ int terrain::taille2()const{
 
 void terrain::lireTerrain(const std::string&nomFichier)
 {
-    //std::ifstream f(nomFichier);
-   // f >> d_nbdebris >>",">> d_joueur;
+     std::ifstream f(nomFichier);
+     char c;
+     f >> d_nbdebris >> c >> d_nbrobotFirstG >> c >> d_nbrobotSecondG >> c >> d_taille1 >> c >> d_taille2;
 }
 
 void terrain::changerTailleGrille (int taille1, int taille2){
