5a98482263c9e276b4c9ec74ca13dd079b347c71
MelLutten <57198988+MelLutten@users.noreply.github.com>
2019-11-01 22:57:47 +0100

    Update fenetre.cpp

--- fenetre.cpp
+++ fenetre.cpp
@@ -10,16 +10,24 @@ fenetre::fenetre(terrain&t):d_t{t}, d_win{this}
 
 void fenetre::vueCreer(terrain&t){
 
+
+
+
     setWindowTitle("HI ROBOTS");
+
     setStyleSheet("QMainWindow{background:#353535;}");
-    
+
     //Terrain
     d_grille = new grille{t};
+    d_grille->setFixedHeight(550);
 
     //Titre
-    auto label = new QLabel{"HI ROBOTS"};
-    label->setFixedSize(maximumWidth(),60);
-    label->setStyleSheet("QLabel { background-color : #2F2F2F; color : white; font-size:20px; }");
+    auto label = new QLabel{this};
+    QPixmap *logo = new QPixmap ("/Users/Neron/Desktop/hirobot.png" );
+    label->setPixmap(*logo);
+
+
+    label->setFixedSize(maximumWidth(),150);
     label->setAlignment(Qt::AlignCenter);
 
     //Boutons niveau
@@ -28,18 +36,26 @@ void fenetre::vueCreer(terrain&t){
     auto normal = new QPushButton{"NORMAL"};
     auto difficile = new QPushButton{"DIFFICILE"};
     auto personnalise = new QPushButton{"PERSONNALISE"};
-    facile->setStyleSheet("QPushButton{ background-color:#242424;font-size:15px;height:30px;color:#6C6C6C;}");
-    normal->setStyleSheet("QPushButton{ background-color:#242424;font-size:15px;height:30px;color:#6C6C6C;}");
-    difficile->setStyleSheet("QPushButton{ background-color:#242424;font-size:15px;height:30px;color:#6C6C6C;}");
-    personnalise->setStyleSheet("QPushButton{ background-color:#242424;font-size:15px;height:30px;color:#6C6C6C;}");
+    facile->setStyleSheet("QPushButton{ background-color:#242424;font-size:11px;height:30px;color:#6C6C6C;}");
+    normal->setStyleSheet("QPushButton{ background-color:#242424;font-size:11px;height:30px;color:#6C6C6C;}");
+    difficile->setStyleSheet("QPushButton{ background-color:#242424;font-size:11px;height:30px;color:#6C6C6C;}");
+    personnalise->setStyleSheet("QPushButton{ background-color:#242424;font-size:11px;height:30px;color:#6C6C6C;}");
     layoutbouton->addWidget(facile);
     layoutbouton->addWidget(normal);
     layoutbouton->addWidget(difficile);
     layoutbouton->addWidget(personnalise);
 
+
      connect(difficile,&QPushButton::clicked,this,&fenetre::OnDifficile);
      connect(normal,&QPushButton::clicked,this,&fenetre::OnNormal);
      connect(facile,&QPushButton::clicked,this,&fenetre::OnFacile);
+
+     //Copyright
+     auto layoutcopy = new QLabel{"© Faculté des Sciences et Techniques de Mulhouse <br> Kolokolnikova - Luttenschlager - Moins - Ndwa"};
+     layoutcopy->setAlignment(Qt::AlignCenter);
+
+
+
 
      //Dialogue personnalisé
      d_p = new personnaliserTerrain{};
@@ -52,6 +68,7 @@ void fenetre::vueCreer(terrain&t){
     layout->addWidget(label,Qt::AlignCenter);
     layout->addLayout(layoutbouton);
     layout->addWidget(d_grille,Qt::AlignCenter);
+    layout->addWidget(layoutcopy,Qt::AlignCenter);
 
 
 
@@ -64,6 +81,7 @@ void fenetre::vueCreer(terrain&t){
 
 
 }
+
 
 
 
@@ -96,13 +114,11 @@ void fenetre::OnPerso(){
     d_p->exec();
     d_t = d_p->terr();
     d_t.afficheGrille(); //Test affichage console
+    vueCreer(d_t);
 
-    if(d_t.terrainOk()){
-         vueCreer(d_t);
-    }
+
 
 }
-
 
 
 
