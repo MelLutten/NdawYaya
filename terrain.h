d67fbb6043827f6f5096ecd5875a00ab7f7d760c
MelLutten <57198988+MelLutten@users.noreply.github.com>
2019-11-01 13:38:45 +0100

    Update terrain.h

--- terrain.h
+++ terrain.h
@@ -1,6 +1,3 @@
-#ifndef TERRAIN_H
-#define TERRAIN_H
-
 #include "joueur.h"
 #include "time.h"
 #include <iostream>
@@ -12,18 +9,18 @@ class terrain
 {
     public:
      terrain();
-     terrain(int nbdebris, int nbrobotfirstG, int nbrobotsecondG, int taille1, int taille2);
-    
+     terrain(int nbdebris, int nbrobotfirstG, int nbrobotsecondG, int nbligne, int nbcolonne);
+
      int nbColonne()const;
      int nbLigne()const;
      int nbDebris()const;
      int nbRobot1G()const;
      int nbRobot2G()const;
      std::vector<std::vector<int>> grille()const;
-    
+
      void lireTerrain(const std::string&nomFichier);
      void sauverTerrain(const std::string&nomFichier);
-     void changerTailleGrille(int taille1, int taille2);
+     void changerTailleGrille(int nbligne, int nbcolonne);
      void changerNb(int nbdebris,int nbRobot1G,int nbRobot2G);
      void InitialisationGrille(int nbdebris, int nbRobot1G, int nbRobot2G);
      bool terrainOk();
@@ -34,8 +31,8 @@ class terrain
     // joueur d_joueur;
      int d_nbrobotFirstG;
      int d_nbrobotSecondG;
-     int d_taille1;
-     int d_taille2;
+     int d_nbligne;
+     int d_nbcolonne;
      std::vector<std::vector<int>> d_grille; // 0 = vide, 1 = joueur, 2 = robot1G, 3 = robot2G, 4 = debris
 
 
