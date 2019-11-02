d2f2a8761845b35ba088729e378862bc5852a9ed
MelLutten <57198988+MelLutten@users.noreply.github.com>
2019-11-02 22:04:13 +0100

    Update fenetre.cpp

--- fenetre.cpp
+++ fenetre.cpp
@@ -19,7 +19,7 @@ void fenetre::vueCreer(terrain&t){
 
     //Terrain
     d_grille = new grille{t};
-    d_grille->setFixedHeight(550);
+    d_grille->setFixedHeight(500);
 
     //Titre
     auto label = new QLabel{this};
@@ -50,6 +50,24 @@ void fenetre::vueCreer(terrain&t){
      connect(normal,&QPushButton::clicked,this,&fenetre::OnNormal);
      connect(facile,&QPushButton::clicked,this,&fenetre::OnFacile);
 
+     //Boutons fermer charger et sauvegarder
+     auto layoutbout = new QHBoxLayout{};
+     auto sauver = new QPushButton{"SAUVEGARDER"};
+     auto charge = new QPushButton{"CHARGER"};
+     auto quit = new QPushButton{"QUITTER"};
+     sauver->setStyleSheet("QPushButton{ background-color:#242424;font-size:11px;height:30px;color:#6C6C6C;}");
+     charge->setStyleSheet("QPushButton{ background-color:#242424;font-size:11px;height:30px;color:#6C6C6C;}");
+     quit->setStyleSheet("QPushButton{ background-color:#242424;font-size:11px;height:30px;color:#6C6C6C;}");
+     personnalise->setStyleSheet("QPushButton{ background-color:#242424;font-size:11px;height:30px;color:#6C6C6C;}");
+     layoutbout->addWidget(sauver);
+     layoutbout->addWidget(charge);
+     layoutbout->addWidget(quit);
+
+     connect(quit,&QPushButton::clicked,this,&fenetre::OnQuit);
+     connect(sauver,&QPushButton::clicked,this,&fenetre::OnSave);
+     connect(charge,&QPushButton::clicked,this,&fenetre::OnCharge);
+
+
      //Copyright
      auto layoutcopy = new QLabel{"© Faculté des Sciences et Techniques de Mulhouse <br> Kolokolnikova - Luttenschlager - Moins - Ndwa"};
      layoutcopy->setAlignment(Qt::AlignCenter);
@@ -68,6 +86,7 @@ void fenetre::vueCreer(terrain&t){
     layout->addWidget(label,Qt::AlignCenter);
     layout->addLayout(layoutbouton);
     layout->addWidget(d_grille,Qt::AlignCenter);
+    layout->addLayout(layoutbout,Qt::AlignCenter);
     layout->addWidget(layoutcopy,Qt::AlignCenter);
 
 
@@ -126,6 +145,21 @@ void fenetre::OnPerso(){
 
 }
 
+void fenetre::OnQuit(){
+    close();
+}
 
+void fenetre::OnSave(){
 
+    /*std::string nomFichier;
+    std::cout<<"Nom Fichier : ";
+    std::cin>>nomFichier;*/
+    d_t.sauverTerrain("/Users/Neron/Desktop/sauv.txt");
+    QMessageBox sauv;
+    sauv.setText("Fichier sauvegardé !");
+    sauv.exec();
 
+}
+void fenetre::OnCharge(){
+
+}
